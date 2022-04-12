35e1c1a15452801c27004cba9132f6ef76c33ad5
nimetapaydin <nimetapaydin@gmail.com>
2022-04-12 18:49:50 +0300

    veri saklama silme user defaults

--- layout/LayoutProject/LayoutProject/ViewController.swift
+++ layout/LayoutProject/LayoutProject/ViewController.swift
@@ -14,10 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var birinciLabel: UILabel!
     
     @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var neyapacaksiniz: UITextField!
+    
+    @IBOutlet weak var nezaman: UITextField!
+    
+    @IBOutlet weak var yapilacakis: UILabel!
+    
+    @IBOutlet weak var yapilacakzaman: UILabel!
+    
     var alinansifre = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let kaydedilenNot = UserDefaults.standard.object(forKey: "not")
+        let kaydedilenZaman = UserDefaults.standard.object(forKey: "zaman")
+        
+        // as -> casting
+        // as? yüzde yüz string , as! string olabilir
+        
+        if let gelenNot  = kaydedilenNot as? String {
+            yapilacakis.text = "Yapılacak İş: \(gelenNot)"
+        }
+        
+        if let gelenZaman = kaydedilenZaman as? String {
+            yapilacakzaman.text = "Yapilacak Zaman: \(gelenZaman)"
+        }
     }
     
     
@@ -30,9 +53,6 @@ class ViewController: UIViewController {
         }else{
             birinciLabel.text = "Şifreniz Yanlıştır"
         }
-        
-       
-        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -42,5 +62,34 @@ class ViewController: UIViewController {
         
         }
     }
+    
+    @IBAction func kaydettiklandi(_ sender: Any) {
+        
+        UserDefaults.standard.set(neyapacaksiniz.text!, forKey: "not")
+        UserDefaults.standard.set(nezaman.text!, forKey: "zaman")
+        
+        yapilacakis.text = "Yapılacak İş: \(neyapacaksiniz.text!)"
+        yapilacakzaman.text = "Yapılacak Zaman: \(nezaman.text!)"
+    }
+    
+    
+    @IBAction func silTiklandi(_ sender: Any) {
+      let kaydedilenNot = UserDefaults.standard.object(forKey: "not")
+        
+      let kaydedilenZaman = UserDefaults.standard.object(forKey: "zaman")
+        
+        if (kaydedilenNot as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "not")
+            yapilacakis.text = "Yapilacak iş: "
+        
+         }
+        
+        if (kaydedilenZaman as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "zaman")
+            yapilacakzaman.text = "Yapilacak Zaman :"
+            
+        }
+    }
+    
 }
 
