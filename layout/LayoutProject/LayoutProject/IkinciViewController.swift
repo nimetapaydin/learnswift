59a6826bd541f4ace9326c1b0145210f6c292e70
nimetapaydin <nimetapaydin@gmail.com>
2022-04-14 18:38:57 +0300

    IkinciViewController Updated.

--- layout/LayoutProject/LayoutProject/IkinciViewController.swift
+++ layout/LayoutProject/LayoutProject/IkinciViewController.swift
@@ -30,5 +30,37 @@ class IkinciViewController: UIViewController {
     
 
     @IBAction func signUpTiklandi(_ sender: Any) {
+        
+        if emailTextField.text == "" {
+            //email ini girmemişsin
+            alertOlustur(titleGirdisi: "Hata!", messageGirdisi: "Email eksik")
+        } else if passwordTextField.text == "" {
+            //parolanı girmemişsin
+            alertOlustur(titleGirdisi: "Hata!", messageGirdisi: "Parola Eksik")
+        } else if passwordTextField.text != password2TextField.text{
+            //parolalar uyuşmuyor
+            alertOlustur(titleGirdisi: "Hata!", messageGirdisi: "Parolalar uyuşmuyor")
+        } else {
+            //başarılı kayıt ettik seni
+            alertOlustur(titleGirdisi: "Tebrikler!", messageGirdisi: "Kullanıcı oluşturuldu")
+        }
+        
+        
+        
+        func alertOlustur(titleGirdisi: String, messageGirdisi: String){
+            let uyariMesaji = UIAlertController(title:titleGirdisi , message: messageGirdisi, preferredStyle: UIAlertController.Style.alert)
+            
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
+                //OK Buttonuna tıklanınca olacaklar
+                
+                print("ok button tiklandi")
+            }
+            
+            uyariMesaji.addAction(okButton)
+            self.present(uyariMesaji, animated: true, completion: nil)
+        }
+        
+        
+        
     }
 }
