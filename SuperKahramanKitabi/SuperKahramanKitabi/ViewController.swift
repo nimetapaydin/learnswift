777ccb079c3bc2d7fcd57023eecbf07aff04c754
nimetapaydin <nimetapaydin@gmail.com>
2022-04-16 22:55:17 +0300

    SuperKahramanKitabi added.

---
+++ SuperKahramanKitabi/SuperKahramanKitabi/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SuperKahramanKitabi
+//
+//  Created by NİMET APAYDIN on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
