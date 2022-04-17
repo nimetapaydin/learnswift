13473af1bf65cb44d540fa00240584e9b80972e7
nimetapaydin <nimetapaydin@gmail.com>
2022-04-17 19:02:21 +0300

    SuperKahramanKitabi finished.

---
+++ SuperKahramanKitabi/SuperKahramanKitabi/DetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailsViewController.swift
+//  SuperKahramanKitabi
+//
+//  Created by NİMET APAYDIN on 17.04.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+   
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    var secilenKahramanIsmi = ""
+    var secilenKahramanGorselIsmi = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.image = UIImage(named: secilenKahramanGorselIsmi)
+        label.text = secilenKahramanIsmi
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SuperKahramanKitabi/SuperKahramanKitabi/ViewController.swift
+++ SuperKahramanKitabi/SuperKahramanKitabi/ViewController.swift
@@ -7,16 +7,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     @IBOutlet weak var tableView: UITableView!
+    var superKahramanIsimleri = [String]()
+    var superKahramanGorselIsimleri = [String]()
+    var secilenIsim = ""
+    var secilenGorsel = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        superKahramanIsimleri.append("Batman")
+        superKahramanIsimleri.append("Superman")
+        superKahramanIsimleri.append("Iron Man")
+        superKahramanIsimleri.append("Spiderman")
+        superKahramanIsimleri.append("Captain America")
+        
+        
+        superKahramanGorselIsimleri.append("batman")
+        superKahramanGorselIsimleri.append("superman")
+        superKahramanGorselIsimleri.append("ironman")
+        superKahramanGorselIsimleri.append("spiderman")
+        superKahramanGorselIsimleri.append("captainamerica")
+    }
+    
+    //numberOfRowsInSection -> kaç tane row olacak
+    //cellForRow atIndexPath -> hücrenin içirisinde neler gösterilecek
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return superKahramanIsimleri.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = superKahramanIsimleri[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            superKahramanGorselIsimleri.remove(at: indexPath.row)
+            superKahramanIsimleri.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         secilenIsim = superKahramanIsimleri[indexPath.row]
+         secilenGorsel = superKahramanGorselIsimleri[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsViewController
+            destinationVC.secilenKahramanIsmi = secilenIsim
+            destinationVC.secilenKahramanGorselIsmi = secilenGorsel
+        }
+    }
 
 }
 
