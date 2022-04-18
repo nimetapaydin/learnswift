41b65e0fddea1fd93a852adc23db2b0a4e5a5eaf
nimetapaydin <nimetapaydin@gmail.com>
2022-04-18 22:11:00 +0300

    Alisveris Listesi added.

---
+++ AlisVerisListesi/AlisVerisListesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlisVerisListesi
+//
+//  Created by NİMET APAYDIN on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
